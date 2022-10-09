c5c34db14eb906873ff95abf2d86025bc0c1b45e
Willow233 <1642974246@qq.com>
2022-10-09 21:00:23 +0800

    excel

--- src/components/UploadExcel/index.vue
+++ src/components/UploadExcel/index.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
+  <div class="upload-excel">
+    <div class="btn-upload">
+      <el-button :loading="loading" size="mini" type="primary" @click="handleUpload">
+        点击上传
+      </el-button>
+    </div>
+
     <input ref="excel-upload-input" class="excel-upload-input" type="file" accept=".xlsx, .xls" @change="handleClick">
     <div class="drop" @drop="handleDrop" @dragover="handleDragover" @dragenter="handleDragover">
-      Drop excel file here or
-      <el-button :loading="loading" style="margin-left:16px;" size="mini" type="primary" @click="handleUpload">
-        Browse
-      </el-button>
+      <i class="el-icon-upload" />
+      <span>将文件拖到此处</span>
     </div>
   </div>
 </template>
 
 <script>
+// npm i xlsx 安装xlsx包
 import XLSX from 'xlsx'
 export default {
   props: {
@@ -115,21 +120,34 @@ export default {
 }
 </script>
 
-<style scoped>
-.excel-upload-input{
-  display: none;
-  z-index: -9999;
-}
-.drop{
-  border: 2px dashed #bbb;
-  width: 600px;
-  height: 160px;
-  line-height: 160px;
-  margin: 0 auto;
-  font-size: 24px;
-  border-radius: 5px;
-  text-align: center;
-  color: #bbb;
-  position: relative;
+<style scoped lang="scss">
+.upload-excel {
+  display: flex;
+  justify-content: center;
+   margin-top: 100px;
+   .excel-upload-input{
+       display: none;
+        z-index: -9999;
+     }
+   .btn-upload , .drop{
+      border: 1px dashed #bbb;
+      width: 350px;
+      height: 160px;
+      text-align: center;
+      line-height: 160px;
+   }
+   .drop{
+       line-height: 80px;
+       color: #bbb;
+      i {
+        margin-top: 40px;
+        font-size: 60px;
+        display: block;
+      }
+      span {
+        display: block;
+        line-height: 20px;
+      }
+   }
 }
 </style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 // 负责所有全局自定义组件的注册
 import PageTools from './PageTools/index.vue'
+import UploadExcel from './UploadExcel/index.vue'
 export default {
   install(Vue) {
     // 组件的注册
     Vue.component('PageTools', PageTools)
+    Vue.component('UploadExcel', UploadExcel)
   }
 }
 
